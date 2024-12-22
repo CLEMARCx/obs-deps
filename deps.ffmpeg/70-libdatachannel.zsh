3430301dcb9b674260c36f2ba42458d2481ecb3b
CLEMARCx <fabi@locker.cc>
2024-12-22 22:49:05 +0100

    Update 70-libdatachannel.zsh

--- deps.ffmpeg/70-libdatachannel.zsh
+++ deps.ffmpeg/70-libdatachannel.zsh
@@ -2,7 +2,7 @@ autoload -Uz log_debug log_error log_info log_status log_output
 
 ## Dependency Information
 local name='libdatachannel'
-local version='v0.21.0'
+local version='v0.22.3'
 local url='https://github.com/paullouisageneau/libdatachannel.git'
 local hash='9d5c46b8f506943727104d766e5dad0693c5a223'
 
